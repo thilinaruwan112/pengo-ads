bb6e83b65e30555bb3296325e9305ff864e59961
Thilina Ruwan <thilinaruwan112@gmail.com>
2025-07-23 18:01:04 +0000

    https://www.svgrepo.com/download/402309/penguin.svg
    can you use this

--- src/components/ui/spinner.tsx
+++ src/components/ui/spinner.tsx
@@ -16,9 +16,9 @@ const PenguinIcon = ({ className }: { className?: string }) => (
         strokeLinejoin="round"
         className={className}
     >
-        <path d="M12 10a2 2 0 0 0-2-2H8a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h2a2 2 0 0 0 2-2Z"/>
+        <path d="M12 10a2 2 0 0 0-2-2H8a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h2a2 2 0 0 0 2-2Z" fill="currentColor"/>
         <path d="M11 14V9a1 1 0 0 1 1-1h.5a1 1 0 0 1 1 1v2"/>
-        <path d="M16.5 15.5a2.5 2.5 0 1 1-3.16-3.16"/>
+        <path d="M16.5 15.5a2.5 2.5 0 1 1-3.16-3.16" fill="currentColor"/>
         <path d="M18 11.05V19a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/>
     </svg>
 );
